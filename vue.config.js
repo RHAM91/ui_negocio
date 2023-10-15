54a1976698a97cfecd8f4404afd65b4ddf8461de
Romario Torres <cs360.x@hotmail.com>
2023-10-15 17:43:29 -0600

    cambis de ruta y directorios

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/pages/tienda'
+  publicPath: '/pages/tienda/consulta'
 })
